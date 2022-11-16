bae9234c1f12ea7064a8ed2b80cf9553a82e3711
William Skou Heidemann <wihe@itu.dk>
2022-11-16 15:45:57 +0100

    more node logic

--- Server/Node.go
+++ Server/Node.go
@@ -6,7 +6,10 @@ import (
 	"sync"
 )
 
-var highestBid = 0
+var highestBid int32 = 0
+var highestBidder string = ""
+var highestBidderID int32 = 0
+
 var mutex = sync.Mutex{}
 var port = flag.Int("port", 8080, "server port number")
 
@@ -18,12 +21,36 @@ func auctionIsOver() {
 
 }
 
-func updateHighestBid(bid proto.BidRequest) proto.Ack {
-	if highestBid < bid.Amount {
-
+func evaluateNewBid(bid *proto.BidRequest) proto.Ack {
+	if isWinningBet(bid) {
+		updateHighestBid(bid)
+		return proto.Ack{}
 	}
+	return proto.Ack{}
 }
 
-func result() proto.BidRequest {
+func isWinningBet(bid *proto.BidRequest) bool {
+	if bid.Amount > highestBid {
+		return true
+	}
+	if bid.Amount == highestBid && bid.ProcessID < highestBidderID {
+		return true
+	}
+	return false
+}
 
+func updateHighestBid(bid *proto.BidRequest) {
+	mutex.Lock()
+	highestBid = bid.Amount
+	highestBidder = bid.Name
+	mutex.Unlock()
+}
+
+func result() *proto.BidResult {
+	result := &proto.BidResult{
+		Amount:        highestBid,
+		Name:          highestBidder,
+		AuctionStatus: "Ongoing",
+	}
+	return result
 }
