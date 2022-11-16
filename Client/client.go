d1a89ae854ab11e6f8cd9980138b0db28e32dbee
Silke Bonnen <silke.bonnen@gmail.com>
2022-11-16 15:38:39 +0100

    proto change

--- Client/client.go
+++ Client/client.go
@@ -1,7 +1,19 @@
-package Client
+package main
 
 import (
+	"AuctionReplication/proto"
+	"bufio"
 	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/MadsRoager/AuctionReplication/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type Client struct {
@@ -10,11 +22,55 @@ type Client struct {
 }
 
 var name = flag.String("clientName", "DefaultName", "client name")
-var id = flag.Int()
+var id = flag.Int("id", 0, "id")
+var frontendPort = flag.Int("frontendPort", 8000, "frontend port")
 
 func main() {
 
 	client := &Client{
-		name: *name,
+		name: 	*name,
+		id: 	*id,
+	}
+
+	go startClient(client)
+
+	for {
+		time.Sleep(100 * time.Second)
+	}
+}
+
+func startClient(client *Client) {
+
+	serverConnection := getServerConnection(client)
+
+	sendMessage(serverConnection)	
+
+}
+
+func getServerConnection(client *Client) proto.MessagingServiceClient {
+	conn, err := grpc.Dial(":"+strconv.Itoa(*frontendPort), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalln("Could not dial server")
+	}
+	return proto.NewMessagingServiceClient(conn)
+}
+
+func sendMessage(serverConnection proto.FrontendClient) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		input := scanner.Text()
+		if strings.HasPrefix(input, "bid") {
+			split := strings.Split(input, " ")
+			ans, err := serverConnection.Bid(&proto.BidRequest{
+				Amount: split[1],
+				Name: name,
+				ProcessID: id,
+			})
+
+		} 
+		if input == "result" {
+			ans, err := serverConnection.Result(&proto.Void{})
+			log.Println(ans.Amount)
+		} 
 	}
 }
