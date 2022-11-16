8ad6233a2513160e3d924aeecc1925443b23fd4a
William Skou Heidemann <wihe@itu.dk>
2022-11-16 16:02:45 +0100

    more work on node

--- Server/Node.go
+++ Server/Node.go
@@ -1,13 +1,15 @@
-package Server
+package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/MadsRoager/AuctionReplication/proto"
 	"sync"
+	"time"
 )
 
 var highestBid int32 = 0
-var highestBidder string = ""
+var highestBidder string = "No bidder yet"
 var highestBidderID int32 = 0
 
 var mutex = sync.Mutex{}
@@ -15,18 +17,26 @@ var port = flag.Int("port", 8080, "server port number")
 
 func main() {
 	flag.Parse()
+	defer runAuction()
 }
 
-func auctionIsOver() {
+var counter = 0
+
+func runAuction() {
+	for {
+		time.Sleep(time.Second)
+		fmt.Println(counter)
+		counter++
+	}
 
 }
 
 func evaluateNewBid(bid *proto.BidRequest) proto.Ack {
 	if isWinningBet(bid) {
 		updateHighestBid(bid)
-		return proto.Ack{}
+		return success()
 	}
-	return proto.Ack{}
+	return fail()
 }
 
 func isWinningBet(bid *proto.BidRequest) bool {
@@ -53,4 +63,16 @@ func result() *proto.BidResult {
 		AuctionStatus: "Ongoing",
 	}
 	return result
+}
+
+func success() proto.Ack {
+	return proto.Ack{
+		Ack: "Success",
+	}
+}
+
+func fail() proto.Ack {
+	return proto.Ack{
+		Ack: "Fail",
+	}
 }
