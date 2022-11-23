cf2cc942febb2ea9bd0f46c003300b62ad760111
Mads <mroa@itu.dk>
2022-11-23 13:03:34 +0100

    Refactored Frontend

--- Frontend/frontend.go
+++ Frontend/frontend.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"google.golang.org/grpc/status"
 	"log"
 	"net"
 	"strconv"
@@ -28,7 +29,7 @@ func main() {
 		serverConnection: getServerConnection(),
 	}
 	go startFrontEnd(frontend)
-	
+
 	fmt.Println("frontend started")
 	for {
 		time.Sleep(100 * time.Second)
@@ -63,65 +64,57 @@ func getServerConnection() []proto.ServerClient {
 }
 
 func (frontend *Frontend) Result(ctx context.Context, in *proto.Void) (*proto.BidResult, error) {
-	// send GetHighestBid to all servernodes
 	counter := 0
 	acks := make([]*proto.BidResult, 3)
 	for i := 0; i < 3; i++ {
 		ack, err := frontend.serverConnection[i].GetHighestBid(ctx, in)
-		
+
 		if err == nil {
+			log.Printf("Frontend requested result in node %d\n", i)
 			acks[counter] = ack
 			counter++
 		}
 	}
 	if len(acks) == 0 {
-		return nil, grpc.Errorf(1, "error")
+		return nil, status.Errorf(1, "error")
 	}
 	return acks[0], nil
-	// when receiving a response, return it and wait for the other serverNodes to reply
-
 }
 
 func (frontend *Frontend) Bid(ctx context.Context, in *proto.BidRequest) (*proto.Ack, error) {
-	// send UpdateHighestBid to all servernodes
 	counter := 0
 	acks := make([]*proto.Ack, 3)
-	log.Println("it gets in bid in frontend")
+	log.Printf("Frontend received bid of %d by %s", in.Amount, in.Name)
 	for i := 0; i < 3; i++ {
-		log.Printf("sends updatehighest bid to node %d\n", i) 
 		conn := frontend.serverConnection[i]
 		ack, err := conn.UpdateHighestBid(ctx, in)
-		
+
 		if err == nil {
+			log.Printf("sends updatehighest bid to node %d\n", i)
 			acks[counter] = ack
 			counter++
 		}
 	}
 	if len(acks) == 0 {
-		return nil, grpc.Errorf(1, "error")
+		return nil, status.Errorf(1, "error")
 	}
 	return acks[0], nil
-	// when receiving a response, return it and wait for the other serverNodes to reply
-
 }
 
 func (frontend *Frontend) StartAuction(ctx context.Context, in *proto.Void) (*proto.Ack, error) {
-	// send UpdateHighestBid to all servernodes
 	counter := 0
 	acks := make([]*proto.Ack, 3)
 	for i := 0; i < 3; i++ {
-		log.Printf("sends  to node %d\n", i) 
+
 		ack, err := frontend.serverConnection[i].StartAuction(ctx, in)
-		
 		if err == nil {
+			log.Printf("Frontend started auction in node %d\n", i)
 			acks[counter] = ack
 			counter++
 		}
 	}
 	if len(acks) == 0 {
-		return nil, grpc.Errorf(1, "error")
+		return nil, status.Errorf(1, "error")
 	}
 	return acks[0], nil
-	// when receiving a response, return it and wait for the other serverNodes to reply
-
 }
