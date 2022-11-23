95b907eb14fbb2e0d8abbcce5f0c420dec206007
William Skou Heidemann <wihe@itu.dk>
2022-11-23 13:03:21 +0100

    refactored client into multiple functions

--- Client/client.go
+++ Client/client.go
@@ -15,33 +15,24 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-type Client struct {
-	name string
-	id   int
-}
-
 var name = flag.String("name", "DefaultName", "client name")
 var id = flag.Int("id", 0, "id")
 var frontendPort = flag.Int("frontendPort", 8000, "frontend port")
 
 func main() {
 	flag.Parse()
-	client := &Client{
-		name: *name,
-		id:   *id,
-	}
-	go startClient(client)
+	go startClient()
 	for {
 		time.Sleep(100 * time.Second)
 	}
 }
 
-func startClient(client *Client) {
-	serverConnection := getServerConnection(client)
+func startClient() {
+	serverConnection := getServerConnection()
 	sendMessage(serverConnection)
 }
 
-func getServerConnection(client *Client) proto.FrontendClient {
+func getServerConnection() proto.FrontendClient {
 	conn, err := grpc.Dial(":"+strconv.Itoa(*frontendPort), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalln("Could not dial server")
@@ -57,36 +48,47 @@ func sendMessage(serverConnection proto.FrontendClient) {
 	for scanner.Scan() {
 		input := scanner.Text()
 		if strings.HasPrefix(input, "bid") {
-			split := strings.Split(input, " ")
-			amount, _ := strconv.ParseInt(split[1], 10, 32)
+			postNewBid(serverConnection, input, ctx)
+		} else if input == "result" {
+			logAuctionResult(serverConnection, ctx)
+		} else if input == "start" {
+			startAuction(serverConnection, ctx)
+		} else {
+			log.Println("Unknown command: ", input)
+		}
+	}
+}
 
-			ans, err := serverConnection.Bid(ctx, &proto.BidRequest{
-				Amount:    int32(amount),
-				Name:      *name,
-				ProcessID: int32(*id),
-			})
-			if err != nil {
-				log.Fatal("some error occured")
-			} else {
-				log.Println(ans.Ack)
-			}
+func postNewBid(serverConnection proto.FrontendClient, input string, ctx context.Context) {
+	split := strings.Split(input, " ")
+	amount, _ := strconv.ParseInt(split[1], 10, 32)
 
-		}
-		if input == "result" {
-			ans, err := serverConnection.Result(ctx, &proto.Void{})
-			if err != nil {
-				log.Fatal("some error occured")
-			} else {
-				log.Println(ans.AuctionStatus + " The highest bid is " + strconv.Itoa(int(ans.Amount)) + " by " + ans.Name)
-			}
-		}
-		if input == "start" {
-			ans, err := serverConnection.StartAuction(ctx, &proto.Void{})
-			if err != nil {
-				log.Fatal("Some error occured")
-			} else {
-				log.Println(ans.Ack)
-			}
-		}
+	ans, err := serverConnection.Bid(ctx, &proto.BidRequest{
+		Amount:    int32(amount),
+		Name:      *name,
+		ProcessID: int32(*id),
+	})
+	if err != nil {
+		log.Fatal("some error occured")
+	} else {
+		log.Println(ans.Ack)
+	}
+}
+
+func logAuctionResult(serverConnection proto.FrontendClient, ctx context.Context) {
+	ans, err := serverConnection.Result(ctx, &proto.Void{})
+	if err != nil {
+		log.Fatal("some error occured")
+	} else {
+		log.Println(ans.AuctionStatus + " The highest bid is " + strconv.Itoa(int(ans.Amount)) + " by " + ans.Name)
+	}
+}
+
+func startAuction(serverConnection proto.FrontendClient, ctx context.Context) {
+	ans, err := serverConnection.StartAuction(ctx, &proto.Void{})
+	if err != nil {
+		log.Fatal("Some error occured")
+	} else {
+		log.Println(ans.Ack)
 	}
 }
